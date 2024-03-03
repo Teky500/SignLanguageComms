a768881e9a4d2d4eeddfcf478342200bf13c7210
Teky500 <mohabemad@gmail.com>
2024-03-02 22:21:05 -0400

    backend translation improved

--- src/backend.py
+++ src/backend.py
@@ -10,12 +10,21 @@ def define_map(init_path):
 def translate_words(sentence):
     w_map = define_map('src/images')
     sentence_by_words = sentence.split(' ')
+    print(len(sentence_by_words))
+    counter_1 = 0
+    arr = []
+    for k in range(len(sentence_by_words)):
+        arr.append([])
+    print(arr)
     for i in sentence_by_words:
+        arr[counter_1] = []
         for j in i:
+            print(counter_1)
             capitalLetter = j.upper()
             if capitalLetter not in list(w_map.keys()):
                 pass
             else:
-                print(w_map[capitalLetter])
-
-(translate_words('this is a test!'))
+                arr[counter_1].append(w_map[capitalLetter])
+        counter_1 += 1
+    print(arr)
+    return arr

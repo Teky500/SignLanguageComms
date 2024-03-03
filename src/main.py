d1a52d4ff5d6b3d56ba959ab930c05ea6b215a60
crounic <sabdalla11124@upei.ca>
2024-03-02 23:34:38 -0400

    Implemented speech to text recognition using the speech_to_text.py library. After speaking, it will convert what you said into text and put it into the textbox that is ready for translation.

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.graphics import Color, Rectangle
 import os
 from image_file import create_image_layout
+from speech_to_text import record_and_convert
 
 class ASLInterpreterApp(App):
     def build(self):
@@ -32,6 +33,16 @@ class ASLInterpreterApp(App):
             background_color=(0.9, 0.9, 0.9, 1),  # Light gray background
         )
         layout1.add_widget(self.text_input)
+
+        record_button = Button(
+            text='Record Speech',
+            size_hint=(1, None),
+            height=50,
+            background_color=(0.2, 0.6, 0.8, 1),  # Light blue button color
+            font_size=16,
+        )
+        record_button.bind(on_press=self.on_record)
+        layout1.add_widget(record_button)
 
         translate_button = Button(
             text='Translate to ASL',
@@ -60,6 +71,11 @@ class ASLInterpreterApp(App):
     def on_translate(self, instance):
         # Switch to the new page
         self.sm.current = 'screen2'
+
+    def on_record(self, instance):
+        # Record speech and convert to text
+        text = record_and_convert()
+        self.text_input.text = text
 
 
 if __name__ == '__main__':
